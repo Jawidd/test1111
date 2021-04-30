9c5b4816e7e5a41f4f6d050fab27507bc440b974
Jawidd <jawid00786@gmail.com>
2021-04-30 21:58:52 +0430

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,10 +8,16 @@
       subTitle1="Category"
       subTitle2="item"
       >props Dialog</props-dialog
-    ><a href="https://github.com/Jawidd/test1111">Code link</a>
+    ><a
+      href="https://github.com/Jawidd/test1111/blob/master/src/components/propsDialog.vue"
+      >Code link</a
+    >
     <br />
     <base-dialog class="my-10 mx-4 d-inline-block"> Dialog</base-dialog>
-    <a href="https://github.com/Jawidd/test1111">Code link</a>
+    <a
+      href="https://github.com/Jawidd/test1111/blob/master/src/components/Dialog.vue"
+      >Code link</a
+    >
   </div>
 </template>
 <script>
